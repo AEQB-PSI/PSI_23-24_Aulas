a37346ca385b40dfc56579497cc5e1dd9e976110
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-10-15 15:11:00 +0100

    Adicionar Exercício 2 da Semana 1

---
+++ Semana01/OlaMundo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    /// <summary>
+    /// Este programa imprime apenas 1 mensagem: "Hello World!".
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// O método Main é responsável por imprimir a mensagem.
+        /// </summary>
+        /// <param name="args"> 
+        /// Esta variável guarda os argumentos passados na linha de comandos.
+        /// </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
