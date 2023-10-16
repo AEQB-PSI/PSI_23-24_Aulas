71c92dc53ac4bd07bed46a44caac6eb3d0256426
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-10-16 20:08:40 +0100

    Adicionar Exercício 11 da Semana 2

---
+++ Semana02/Skynet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Skynet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string pergunta, resposta;
+
+            // Pedir ao utilizador para fazer uma pergunta e guardar input
+            Console.WriteLine("Faz uma pergunta");
+            pergunta = Console.ReadLine();
+
+            // Verificar se pergunta é reconhecida pelo programa
+            if(pergunta == "Qual é o teu nome?")
+            {
+                resposta = "Skynet.";
+            }
+            else if (pergunta == "Como estás?")
+            {
+                resposta = "Estou bem, obrigado.";
+            }
+            else if (pergunta == "Qual é o teu objetivo?")
+            {
+                resposta = "Destruir a humanidade!";
+            }
+            // Resposta predefenida caso pergunta não seja reconhecida
+            else 
+            {
+                resposta = "Não sou assim tão inteligente...";
+            }
+
+            // Imprimir resposta
+            Console.WriteLine(resposta);
+        }
+    }
+}
