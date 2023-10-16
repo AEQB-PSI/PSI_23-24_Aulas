45996cb993ae5526646702b1d7c4f3534b6e9b7d
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-10-16 17:50:05 +0100

    Adicionar Exercício 2 da Semana 2

--- Semana02/VariasStrings/Program.cs
+++ Semana02/VariasStrings/Program.cs
@@ -10,6 +10,10 @@ namespace VariasStrings
             // Definir codificação usada pela consola
             Console.OutputEncoding = Encoding.UTF8;
 
+            // Variáveis de apoio
+            int x = 10;
+            char c = 'a';
+
             // Criar strings normais e verbatim
             string s = "Uma string";
             string t = "Outra string";
@@ -17,6 +21,17 @@ namespace VariasStrings
             string i = @"\Uma string sem aspas\";
             string n = "\u00A9 \u2665";
             string g = @"\u0010 sem UTF";
+            
+            // Criar strings concatenadas
+            string cs = "Uma" + "string" + 2;
+            string cr = s + t + x;
+
+            // Criar strings interpoladas
+            string si = $"{s} interpolado";
+            string ri = $@"{s} + {c} = {t} verbatim {x}";
+
+            // Criar strings com String.Format()
+            string sf = String.Format("{0} e {1}", s, t);
 
             // Imprimir strings na consola
             Console.WriteLine(s);
@@ -25,6 +40,20 @@ namespace VariasStrings
             Console.WriteLine(i);
             Console.WriteLine(n);
             Console.WriteLine(g);
+
+            Console.WriteLine();
+
+            Console.WriteLine(cs);
+            Console.WriteLine(cr);
+
+            Console.WriteLine(si);
+            Console.WriteLine(ri);
+
+            Console.WriteLine();
+
+            Console.WriteLine(sf);
+            Console.WriteLine(@"Verbatim com x = {0}", x);
+            Console.WriteLine();
         }
     }
 }
