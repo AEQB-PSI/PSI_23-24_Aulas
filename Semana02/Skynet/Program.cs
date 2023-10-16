f085d6fe5a33c06e1eb7d97a8b9e1714877b73b8
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-10-16 20:18:36 +0100

    Adicionar Exercício 13 da Semana 2

--- Semana02/Skynet/Program.cs
+++ Semana02/Skynet/Program.cs
@@ -8,31 +8,54 @@ namespace Skynet
         {
             // Variáveis auxiliares
             string pergunta, resposta;
+            bool exit = false;
 
-            // Pedir ao utilizador para fazer uma pergunta e guardar input
-            Console.WriteLine("Faz uma pergunta");
-            pergunta = Console.ReadLine();
-
-            // Verificar se pergunta é reconhecida pelo programa
-            switch (pergunta)
+            // Ciclo DO-WHILE executa enquanto não se verificar condição de fim
+            do 
             {
-                case "Qual é o teu nome?":
-                    resposta = "Skynet.";
-                    break;
-                case "Como estás?":
-                    resposta = "Estou bem, obrigado.";
-                    break;
-                case "Qual é o teu objetivo?":
-                    resposta = "Destruir a humanidade!";
-                    break;
-                // Resposta predefinida caso programa não reconheça a pergunta
-                default:
-                    resposta = "Não sou assim tão inteligente...";
-                    break;
+                // Pedir ao utilizador para fazer uma pergunta e guardar input
+                Console.WriteLine("Faz uma pergunta");
+                pergunta = Console.ReadLine();
+                
+                // Verificar se input do utilizador pede para terminar programa
+                if (pergunta == "EXIT")
+                {
+                    // Definir condições para terminar programa
+                    exit = true;
+                    resposta = "";
+                }
+                // Executar o programa normalmente caso não existam condições
+                // para terminar a execução
+                else
+                {
+                    // Verificar se pergunta é reconhecida pelo programa
+                    switch (pergunta)
+                    {
+                        case "Qual é o teu nome?":
+                            resposta = "Skynet.";
+                            break;
+                        case "Como estás?":
+                            resposta = "Estou bem, obrigado.";
+                            break;
+                        case "Qual é o teu objetivo?":
+                            resposta = "Destruir a humanidade!";
+                            break;
+                        // Resposta predefinida caso programa não reconheça 
+                        // a pergunta
+                        default:
+                            resposta = "Não sou assim tão inteligente...";
+                            break;
+                    }
+                }
+                
+                // Imprimir resposta caso o programa não vá terminar
+                if (!exit)
+                {
+                    Console.WriteLine(resposta);
+                    Console.WriteLine();
+                }      
             }
-
-            // Imprimir resposta
-            Console.WriteLine(resposta);
+            while (!exit);
         }
     }
 }
