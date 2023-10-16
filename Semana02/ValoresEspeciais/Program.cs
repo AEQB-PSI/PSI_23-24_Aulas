2cd888aa91e70d5a3a1ee4797918d2eb5d05c759
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-10-16 19:57:43 +0100

    Adicionar Exercício 8 da Semana 2

---
+++ Semana02/ValoresEspeciais/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Imprimir valores máximos e mínimos de tipos de valor conhecidos
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(char.MaxValue);
+            Console.WriteLine(char.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+            
+            // Imprimir valores especiais a partir de float
+            Console.WriteLine(float.PositiveInfinity);
+            Console.WriteLine(float.NegativeInfinity);
+            Console.WriteLine(float.PositiveInfinity / float.PositiveInfinity);
+
+            // Imprimir valores especiais a partir de double
+            Console.WriteLine(double.PositiveInfinity);
+            Console.WriteLine(double.NegativeInfinity);
+            Console.WriteLine(double.NaN);
+        }
+    }
+}
