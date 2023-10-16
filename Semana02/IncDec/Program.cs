f187c513a7b64843ea187002831a438e18970486
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-10-16 17:57:05 +0100

    Adicionar Exercício 4 da Semana 2

---
+++ Semana02/IncDec/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 2, c, d;
+
+            // Incrementa 'a' e 'b', só atualiza depois da operação
+            // a = 5 + 1; b = 2 + 1; c = 5 + 2
+            c = a++ + b++; // Depois desta operação, 'a' = 6 e 'b' = 3 
+            Console.WriteLine(c);
+
+            // Decrementa 'a' e 'b' antes da operação
+            // a = 6 - 1; b = 3 - 1; c = 5 + 2
+            d = --a + --b;
+            Console.WriteLine(d);
+        }
+    }
+}
