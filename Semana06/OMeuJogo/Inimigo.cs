cd80a32477f4b51d90906db1b646da5e50179569
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-11-13 09:05:36 +0000

    Adicionar Exercício 4 da Semana 6

--- Semana06/OMeuJogo/Inimigo.cs
+++ Semana06/OMeuJogo/Inimigo.cs
@@ -11,7 +11,7 @@ namespace OMeuJogo
         // Construtor com definição do nome da instância
         public Inimigo(string nome)
         {
-            this.nome = nome;
+            SetNome(nome);
             vida = 100;
         }
 
@@ -19,6 +19,21 @@ namespace OMeuJogo
         public string GetNome()
         {
             return nome;
+        }
+
+        // Método que retorna vida da instância
+        public float GetVida()
+        {
+            if (vida < 50) vida = 50;
+            return vida;
+        }
+
+        // Método que especifica novo nome para a instância
+        public void SetNome(string novoNome)
+        {
+            // Retirar espaços do início e fim da string contendo o novo nome
+            string trimNome = novoNome.Trim();
+            this.nome = trimNome;
         }
 
         // Método para danificar instância com valor dado
