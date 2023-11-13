b611e8f713bdfaf8c6964e1ea9a3317c5ecc4f14
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2023-11-13 09:02:53 +0000

    Adicionar Exercício 2 da Semana 6

--- Semana06/OMeuJogo/Inimigo.cs
+++ Semana06/OMeuJogo/Inimigo.cs
@@ -4,6 +4,15 @@ namespace OMeuJogo
 {
     public class Inimigo
     {
-        
+        // Variáveis de instância
+        private string nome;
+        private float vida;
+
+        // Construtor com definição do nome da instância
+        public Inimigo(string nome)
+        {
+            this.nome = nome;
+            vida = 100;
+        }
     }
 }
