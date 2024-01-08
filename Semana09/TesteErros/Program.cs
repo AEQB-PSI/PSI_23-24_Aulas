bc01629b85e72fe5d37f0dd2d3d1d91a0f08e46a
Replit user <>
2024-01-08 12:37:24 +0000

    Adicionar Exercício 4 da Semana 9

--- Semana09/TesteErros/Program.cs
+++ Semana09/TesteErros/Program.cs
@@ -6,8 +6,21 @@ namespace TesteErros
     {
         static void Main(string[] args)
         {
+          int i = 0;
+          
           Console.Write("Insere um número inteiro: ");
-          int i = Convert.ToInt32(Console.ReadLine());
+          
+          try
+          {
+            // Código que pode lançar exceções
+            i = Convert.ToInt32(Console.ReadLine());
+          }
+          catch (FormatException e)
+          {
+            // Código para tratar exceção
+            Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+          }
+          
           Console.WriteLine($"Número inserido: {i}");
         }
     }
